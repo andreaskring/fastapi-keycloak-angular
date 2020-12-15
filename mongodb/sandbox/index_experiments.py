3c4d037fdcc1c40a67126c51a20421de778f5919
Andreas Kring <andreas@magenta.dk>
2020-12-15 22:31:18 +0100

    MongoDB experiments

---
+++ mongodb/sandbox/index_experiments.py
@@ -0,0 +1,65 @@
+from pprint import pprint
+import pymongo
+
+
+DBNAME = 'experiments'
+
+client = pymongo.MongoClient('mongodb://localhost:27017')
+if DBNAME in client.list_database_names():
+    client.drop_database(DBNAME)
+db = client[DBNAME]
+col_users = db['users']
+
+users = [
+    {
+        'name': 'bruce',
+        'canTeach': [
+            {'name': 'kung fu'},
+            {'name': 'karate'}
+        ],
+        'wantsToLearn': [
+            {'name': 'cooking'},
+            {'name': 'python'}
+        ]
+    },
+    {
+        'name': 'chuck',
+        'canTeach': [
+            {'name': 'judo'},
+            {'name': 'karate'}
+        ],
+        'wantsToLearn': [
+            {'name': 'cooking'},
+            {'name': 'java'}
+        ]
+    },
+    {
+        'name': 'sylvester',
+        'canTeach': [
+            {'name': 'boxing'},
+            {'name': 'survival'}
+        ],
+        'wantsToLearn': [
+            {'name': 'java'},
+            {'name': 'python'}
+        ]
+    },
+    {
+        'name': 'jean',
+        'canTeach': [
+            {'name': 'martial arts'},
+            {'name': 'fighting'}
+        ],
+        'wantsToLearn': [
+            {'name': 'boxing'},
+            {'name': 'java'}
+        ]
+    }
+]
+
+r = col_users.insert_many(users)
+
+# Documents examined without index
+
+r = col_users.find({'canTeach.name': 'karate'}).explain()
+print('totalDocsExamined:', r['executionStats']['totalDocsExamined'])
